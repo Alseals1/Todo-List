bb099f610495526dc29d5e6dbb804e86c555b409
Alandis Seals <alandis.seals@yahoo.com>
2020-09-06 17:05:59 -0500

    Added a div with a li and 2 button

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
 // Selector 
+const todoInput = document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
+
+
 
 // Event Listeners 
+todoButton.addEventListener('click' , addTodo);
+
+
+
 
 //Functions
+
+
+function addTodo(event) {
+   // Prevent form from submiting
+   event.preventDefault()
+
+   //Todo Div
+   const todoDiv = document.createElement('div');
+   todoDiv.classList.add("todo")
+   //Create LI
+   const newTodo = document.createElement('li');
+   newTodo.innerText = 'hey!';
+   newTodo.classList.add('todo-item');
+   todoDiv.appendChild(newTodo);
+
+
+   //Check Mark Button
+   const completedButton = document.createElement('button');
+   completedButton.innerHTML = '<i class="fas fa-check"></i>'
+   completedButton.classList.add("complete-btn")
+   todoDiv.appendChild(completedButton);
+
+   
+   //Check Trash Button
+   const trashButton = document.createElement('button');
+   trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+   trashButton.classList.add("complete-btn")
+   todoDiv.appendChild(trashButton);
+   //Append To List
+   todoList.appendChild(todoDiv)
+}
