94772f445b0a4f1f956c2d41ece3dad6e896bf2f
Alandis Seals <alandis.seals@yahoo.com>
2020-09-07 12:17:32 -0500

    Made the option tab functionable

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 // Event Listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
-todoList.addEventListener("click", completeTodo)
+todoList.addEventListener("click", completeTodo);
+filterOption.addEventListener("click", filterTodo);
 
 //Functions
 
@@ -45,10 +47,10 @@ function deleteCheck(e) {
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
     //Animation
-    todo.classList.add("fall")
-    todo.addEventListener('transitionend', () =>{
-      todo.remove()
-    })
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", () => {
+      todo.remove();
+    });
   }
 }
 
@@ -59,4 +61,30 @@ function completeTodo(e) {
     todo.style.textDecoration = "line-through";
     todo.style.opacity = "0.2";
   }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  // console.log(e.target.value)
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
 }
